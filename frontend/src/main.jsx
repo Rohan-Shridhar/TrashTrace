9d58d8b4122ab44a6e73792394e26cf8832d16bb
Rohan S Mirjankar <rohansm668@gmail.com>
2026-09-23 17:58:44 +0530

    refactor main.jsx to use StrictMode and update import statements

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from "./App";
+
+import "./styles/globals.css";
+import "./styles/utilities.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
